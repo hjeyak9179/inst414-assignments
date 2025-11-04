e241a47351078e2fdeef2ed2464d5fe2764969af
hjeyak9179 <harshyjey@gmail.com>
2025-11-04 16:50:46 -0500

    Module 4 code and dataset

---
+++ module4/Module4Assignment.py
@@ -0,0 +1,31 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import DistanceMetric
+
+# Prepare dataset by mapping values and normalizing 
+plants_df = pd.read_csv("Indoor_Plant_Health_and_Growth_Factors.csv")[["Height_cm", "New_Growth_Count", "Pest_Severity"]].dropna()
+
+severity_map = {"None": 0, "Low": 1, "Moderate": 2, "High": 3}
+plants_df["Pest_Severity"] = plants_df["Pest_Severity"].map(severity_map)
+
+scaler = MinMaxScaler()
+
+normal_plants_df = pd.DataFrame(scaler.fit_transform(plants_df), columns=plants_df.columns)
+
+# Elbow method to determine number of clusters
+elbow = []
+for k in range(1, 20):
+    kmeans = KMeans(n_clusters = k, init = 'k-means++').fit(normal_plants_df)
+    elbow.append(kmeans.inertia_)
+    
+plt.plot(range(1, 20), elbow)
+# shows that the ideal k is 5
+
+# using k groups, executing kmeans
+plant_kmeans = KMeans(n_clusters = 5, init = 'k-means++').fit(normal_plants_df)
+normal_plants_df.insert(0, "Plant_ID", pd.read_csv("Indoor_Plant_Health_and_Growth_Factors.csv")[["Plant_ID"]])
+normal_plants_df['Cluster'] = plant_kmeans.labels_
+normal_plants_df.sort_values(by='Cluster').to_csv('output.csv', index=False)
